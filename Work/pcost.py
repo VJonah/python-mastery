a9e582f82f3263ffd2233b7e39bcc5c476e87b26
Jonah <uclqjv0@ucl.ac.uk>
2023-10-17 13:23:04 +0100

    Exercise 1.4 done.

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,10 +2,18 @@
 
 
 path = '../Data/portfolio.dat'
-total_cost = 0
 
-with open(path,'r') as f:
-    for row in f:
-        _, shares, price = row.split(' ')
-        total_cost += int(shares) * float(price)
-print(total_cost)
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename,'r') as f:
+        for row in f:
+            _, shares, price = row.split()
+            try:
+                total_cost += int(shares) * float(price)
+            # catches the failed int() and float() conversions above
+            except ValueError as e:
+                print(f"Couldn't parse: {repr(row)}")
+                print(f"Reason: {e}")
+    return total_cost
+
+print(portfolio_cost('../Data/portfolio3.dat'))
