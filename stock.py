baac9961ff20d245ee9fba938c8162c89c0bb737
Jonah <uclqjv0@ucl.ac.uk>
2023-11-21 09:07:11 +0000

    Exercise 3.1 completed.

--- stock.py
+++ stock.py
@@ -1,5 +1,6 @@
 # stock.py
 
+
 class Stock:
     def __init__(self,name,shares,price):
         self.name = name
@@ -7,4 +8,36 @@ class Stock:
         self.price = price
 
     def cost(self):
+        '''Returns the cost of the stock.'''
         return self.shares * self.price
+
+    def sell(self,nshares):
+        '''Sells a certain number of shares.'''
+        if nshares > self.shares:
+            raise ValueError("Too few available shares to sell!")
+        self.shares -= nshares
+
+def read_portfolio(file_path):
+    '''
+    Reads a CSV file of stock data into a list of Stocks.
+    '''
+    import csv
+    stocks = []
+    with open(file_path) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            name, shares, price = row
+            s = Stock(name,int(shares),float(price))
+            stocks.append(s)
+    return stocks
+
+def print_portfolio(portfolio):
+    '''
+    Make a nicely formatted table showing stock data.
+    '''
+    print('%10s %10s %10s' % ('name','shares','price'))
+    print(f"{'-' * 10} {'-' * 10} {'-' * 10}")
+    #print(('-' * 10 + ' ') * 3)
+    for s in portfolio:
+        print('%10s %10d %10.2f' % (s.name,s.shares,s.price))
