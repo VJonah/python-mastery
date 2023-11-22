ab022d8191b52846ba7da0d1f189aa83fedda23d
Jonah <uclqjv0@ucl.ac.uk>
2023-11-22 13:27:14 +0000

    Exercise 3.4 completed.

--- stock.py
+++ stock.py
@@ -2,7 +2,9 @@
 
 
 class Stock:
-    types = (str, int, float) # a class variable
+    __slots__ = ('name','_shares','_price')
+    _types = (str, int, float) # a class variable
+
     def __init__(self,name,shares,price):
         self.name = name
         self.shares = shares
@@ -10,12 +12,38 @@ class Stock:
 
     @classmethod
     def from_row(cls,row):
-        values = [func(val) for func, val in zip(cls.types, row)]
+        values = [func(val) for func, val in zip(cls._types, row)]
         return cls(*values)
 
+    @property
     def cost(self):
         '''Returns the cost of the stock.'''
         return self.shares * self.price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self,value):
+        if not isinstance(value,self._types[1]):
+            raise TypeError(f"Expected {self._types[1].__name__}")
+        elif value < 0:
+            raise ValueError("shares must be >= 0")
+        self._shares = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self,value):
+        if not isinstance(value,self._types[2]):
+            raise TypeError(f"Expected {self._types[2].__name__}")
+        elif value < 0:
+            raise ValueError("price must be >= 0")
+        self._price = value
+
 
     def sell(self,nshares):
         '''Sells a certain number of shares.'''
