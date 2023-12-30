b7edb239ec31170b87c97b4fc85b5cebe7c95935
Jonah <uclqjv0@ucl.ac.uk>
2023-12-30 17:33:04 +0000

    Exercises 4.4 and 5.1 completed. This includes the Design and API challenges, although the latter had to be done without looking at the typing module's documentation.

--- reader.py
+++ reader.py
@@ -1,92 +1,43 @@
 # reader.py
-
 import csv
-from abc import ABC, abstractmethod
+from typing import List
 
-
-class DataCollection:
-    def __init__(self,headers):
-        if len(headers) == 0:
-            raise ValueError("DataCollection must be initialised with non-empty 'headers' argument.")
-        for header in headers:
-            setattr(self,header,[])
-
-    def __len__(self):
-        # get the first attr name in the keys of the object's __dict__
-        # we assume all lists have the same length
-        random_attr =  list(self.__dict__)[0]
-        return len(getattr(self,random_attr))
-
-    def __getitem__(self,key):
-        if isinstance(key,slice):
-            dc = DataCollection(list(self.__dict__))
-            # the slice's 'indices' method returns a tuple with the start, step size and end
-            # this can then be unpacked using * to pass them as arguments to 'range'
-            for i in range(*key.indices(len(self))):
-                dc.append(self[i])
-            return dc
-        return {header:value[key] for header,value in self.__dict__.items()}
-
-    def append(self,d):
-        for header, value in d.items():
-            # shouldn't be a performance hit since it's all references in Python
-            getattr(self,header).append(value)
-
-
-class CSVParser(ABC):
-    def parse(self, filename):
-        records = []
-        with open(filename) as f:
-            rows = csv.reader(f)
-            headers = next(rows)
-            for row in rows:
-                record = self.make_record(headers, row)
-                records.append(record)
-        return records
-
-    @abstractmethod
-    def make_record(self, headers, row):
-        pass
-
-class DictCSVParser(CSVParser):
-    def __init__(self, types):
-        self.types = types
-
-    def make_record(self, headers, row):
-        return {name: func(val) for name, func, val in zip(headers, self.types, row) }
-
-class InstanceCSVParser(CSVParser):
-    def __init__(self, cls):
-        self.cls = cls
-
-    def make_record(self, headers, row):
-        return self.cls.from_row(row)
-
-def read_csv_as_dicts(filename,coltypes):
+def read_csv_as_dicts(filename: str, types: List[type], headers: List[str] = None) -> List[dict]:
     '''
-    Parses a csv into a list of dictionaries with a specific
-    type conversion mapping for each column.
+    Read CSV data into a list of dictionaries with optional type conversion
     '''
-    parser = DictCSVParser(coltypes)
-    return parser.parse(filename)
+    with open(filename, 'rt') as file:
+        return csv_as_dicts(file, types, headers=headers)
 
-def read_csv_as_instances(filename, cls):
+def read_csv_as_instances(filename: str, cls: type, headers: List[str] = None) -> List[type]:
     '''
-    Read a CSV file into a list of instances
+    Read CSV data into a list of instances
     '''
-    parser = InstanceCSVParser(cls)
-    return parser.parse(filename)
+    with open(filename, 'rt') as file:
+        return csv_as_instances(file, cls, headers=headers)
 
-def read_csv_as_columns(filename,types=None):
+def csv_as_dicts(lines: iter, types: List[type], headers: List[str] = None) -> List[dict]:
     '''
-    General purpose CSV parser that stores data in a DataCollection object.
+    Parse CSV lines of an iterable object into a list of dictionary records.
     '''
-    assert type(types) != type(None), "Got no types for 'read_csv_as_columsn'"
-    f = open(filename)
-    rows = csv.reader(f)
-    headers = next(rows)
-    dc = DataCollection(headers)
+    records = []
+    rows = csv.reader(lines)
+    if headers is None:
+        headers = next(rows)
     for row in rows:
-        record = { name:func(val) for name, func, val in zip(headers,types,row) }
-        dc.append(record)
-    return dc
+        record = {name: func(val) for name, func, val in zip(headers, types, row)}
+        records.append(record)
+    return records
+
+def csv_as_instances(lines:iter, cls: type, headers: List[str] = None) -> List[type]:
+    '''
+    Parse CSV lines of an iterable object into a list of class instances.
+    '''
+    records = []
+    rows = csv.reader(lines)
+    if headers is None:
+        headers = next(rows)
+    for row in rows:
+        record = cls.from_row(row)
+        records.append(record)
+    return records
