950ff4549ef50014b24e8bc207f8635de2746e92
Jonah <uclqjv0@ucl.ac.uk>
2024-01-02 18:33:03 +0000

    Completed Exercise 5.4 outside of the challenge. Just comitting for posterity.

---
+++ typedproperty.py
@@ -0,0 +1,61 @@
+# typedproperty.py
+
+def typedproperty(name, expected_type):
+    private_name = '_' + name
+
+    @property
+    def value(self):
+        return getattr(self, private_name)
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, expected_type):
+            raise TypeError(f'Expected {expected_type}')
+        setattr(self, private_name, val)
+
+    return value
+
+def String(name):
+    private_name = '_' + name
+
+    @property
+    def value(self):
+        return getattr(self, private_name)
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, str):
+            raise TypeError('Expected string')
+        setattr(self, private_name, val)
+
+    return value
+
+def Integer(name):
+    private_name = '_' + name
+
+    @property
+    def value(self):
+        return getattr(self, private_name)
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, int):
+            raise TypeError('Expected integer')
+        setattr(self, private_name, val)
+
+    return value
+
+def Float(name):
+    private_name = '_' + name
+
+    @property
+    def value(self):
+        return getattr(self, private_name)
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, float):
+            raise TypeError('Expected float')
+        setattr(self, private_name, val)
+
+    return value
