50caf61f83eb7b731eb48856b52e0a6c3d0aebb3
desktop <me@desktop.com>
2024-01-06 16:45:38 +0100

    Some pretty important changes after seeing solution to Exercise 6.1

--- structure.py
+++ structure.py
@@ -5,18 +5,13 @@ class Structure:
         if len(args) != len(self._fields):
             raise TypeError(f'Expected {len(self._fields)} arguments')
 
-        for i, field in enumerate(self._fields):
-            setattr(self, field, args[i])
+        for name, arg in zip(self._fields, args):
+            setattr(self, name, arg)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({','.join([str(v) if not isinstance(v,str) else repr(v) for _, v in self.__dict__.items()])})"
+        return f"{self.__class__.__name__}({','.join(repr(getattr(self,name)) for name in self._fields)})"
 
     def __setattr__(self, name, value):
-        if name[0] != '_' and name not in self._fields:
+        if not name.startswith('_') and name not in self._fields:
             raise AttributeError(f'No attribute {name}')
-        self.__dict__[name] = value
-
-class Stock(Structure):
-    _fields = ('name','shares','price')
-
-s = Stock('GOOG',100,490.1)
+        super().__setattr__(name, value)
