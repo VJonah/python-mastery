d5ed48f2d4d9da24bfae7391b5d26179c15b71ca
desktop <me@desktop.com>
2024-01-06 17:03:46 +0100

    Exercise 6.2 completed.

--- structure.py
+++ structure.py
@@ -1,17 +1,26 @@
 # structure.py
 
-class Structure:
-    def __init__(self, *args):
-        if len(args) != len(self._fields):
-            raise TypeError(f'Expected {len(self._fields)} arguments')
+import sys
 
-        for name, arg in zip(self._fields, args):
-            setattr(self, name, arg)
+class Structure:
+    _fields = () # why is this necessary? In case we forget to initialise one in the subclass?
+
+    @staticmethod
+    def _init():
+        locs = sys._getframe(1).f_locals
+        self = locs.pop('self')
+        for name, val in locs.items():
+            setattr(self, name, val)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({','.join(repr(getattr(self,name)) for name in self._fields)})"
+        return f"{self.__class__.__name__}({', '.join(repr(getattr(self,name)) for name in self._fields)})"
 
     def __setattr__(self, name, value):
         if not name.startswith('_') and name not in self._fields:
             raise AttributeError(f'No attribute {name}')
         super().__setattr__(name, value)
+
+# a note on the __setattr__ method
+# it works because the method resolution order (MRO) of a
+# child to Structure will use the __setattr__ of builtins.object
+# which behaves as expected! Lesson: always consider differing to super()!
