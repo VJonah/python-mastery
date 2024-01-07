36c8feca29e71cdfc4f79fec44ed6fc7d475bed2
desktop <me@desktop.com>
2024-01-07 17:29:05 +0100

    Exercise 6.4 completed.

--- structure.py
+++ structure.py
@@ -6,17 +6,15 @@ import inspect
 class Structure:
     _fields = () # why is this necessary? In case we forget to initialise one in the subclass?
 
-    @staticmethod
-    def _init():
-        locs = sys._getframe(1).f_locals
-        self = locs.pop('self')
-        for name, val in locs.items():
-            setattr(self, name, val)
-
     @classmethod
-    def set_fields(cls):
-        sig = inspect.signature(cls)
-        cls._fields = tuple(sig.parameters)
+    def create_init(cls):
+        argstr = ','.join(cls._fields)
+        code = f'def __init__(self,{argstr}):\n'
+        for name in cls._fields:
+            code += f'    self.{name} = {name}\n'
+        locs = { }
+        exec(code, locs)
+        cls.__init__ = locs['__init__']
 
     def __repr__(self):
         return f"{self.__class__.__name__}({', '.join(repr(getattr(self, name)) for name in self._fields)})"
