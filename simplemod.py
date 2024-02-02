6a679d8aaa87c1e3d1d502019adb6e8096156e59
me <ubuntu@wsl.com>
2024-02-02 16:54:38 +0100

    Exercise 9.1 completed.

---
+++ simplemod.py
@@ -0,0 +1,16 @@
+# simplemod.py
+
+x = 42      # a global variable
+
+
+# a simple function
+def foo():
+    print('x is', x)
+
+# a simple class
+class Spam:
+    def yow(self):
+        print('More Yow!')
+
+# a scripting statement
+print('Loaded simplemod')
